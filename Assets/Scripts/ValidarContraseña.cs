3cda2b0312ec413b3d3bea9bd52c6286eb338725
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-04-06 11:40:00 -0300

    Exercise comprobar contraseña finished

--- Assets/Scripts/ValidarContraseña.cs
+++ Assets/Scripts/ValidarContraseña.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContraseña : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+    
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void validarContrasenia()
     {
-        
+        contraseniaUsuario = ingresoUsuario.text;
+
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
